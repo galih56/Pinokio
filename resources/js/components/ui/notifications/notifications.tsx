8525697763af606071ecd13e9e4cdbe9ee98e880
galih56 <galihindra650@gmail.com>
2025-05-30 23:46:38 +0700

    fully using sonner

--- resources/js/components/ui/notifications/notifications.tsx
+++ resources/js/components/ui/notifications/notifications.tsx
@@ -1,33 +1,23 @@
-import { Notification } from './notification';
-import { useNotifications } from './notifications-store';
 import { Toaster } from "sonner";
 
 export const Notifications = () => {
-  const { notifications, dismissNotification } = useNotifications();
-
   return (
-    <div
-      aria-live="assertive"
-      className="pointer-events-none fixed inset-0 z-[9999] flex flex-col items-end space-y-4 px-4 py-6 sm:items-start sm:p-6"
-    >
-      {notifications.filter(item => !item.toast).map((notification) => (
-        <Notification
-          key={notification.id}
-          notification={notification}
-          onDismiss={dismissNotification}
-        />
-      ))}
-      <Toaster position='top-right' theme='system' 
+      <Toaster position='top-right' theme='system' gap={80}
         duration={5}
         toastOptions={{
           classNames: {
-            toast: "rounded-lg shadow-lg",
-            success: "border-none bg-green-500 text-white hover:bg-green-400",
-            error: "border-none bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-            warning: "border-none bg-yellow-500 text-yellow-900 hover:bg-yellow-400 text-white",
-            info: "border-none bg-blue-500 text-blue-900 hover:bg-blue-400 text-white",
+            title: "text-white",
+            description: "text-white", 
+            toast: "rounded-lg",
+            success: "border-none bg-green-500 text-white ",
+            error: "border-none bg-destructive text-white ",
+            warning: "border-none bg-yellow-500 text-white",
+            info: "border-none bg-blue-500 text-white",
+            actionButton: "bg-white text-black",
+          },
+          style: {
+            color: "white", // Fallback text color
           },
         }}/>
-    </div>
   );
 };
